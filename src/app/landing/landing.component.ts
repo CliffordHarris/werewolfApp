69c6a507e8b042fc43c1d9d301a600399c563ce0
🤷 <dave_spec@yahoo.com>
2022-12-27 00:34:02 -0600

    update to add colors to person

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -10,7 +10,7 @@ import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
 export class LandingComponent implements OnInit {
   newName: string = '';
-  players: string[] = [];
+  players: Array<{name: string, color: string}> = [];
   @ViewChild('name') nameEl: ElementRef | undefined;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
   addOnBlur = true;
@@ -19,37 +19,68 @@ export class LandingComponent implements OnInit {
     console.log(this.router.url)
   }
 
-  savePlayerToLocalStorage = (players: any) => {
+  savePlayerToLocalStorage = (players:any) => {
     localStorage.setItem("players", JSON.stringify(players));
   };
 
   ngOnInit() {
     const storedPlayers = localStorage.getItem("players");
+
     if (storedPlayers) {
-      this.players = JSON.parse(storedPlayers) as string[];
+      this.players = JSON.parse(storedPlayers);
     }
   }
 
+  playerColor() {
+    const colors = [
+      '#8C6985',
+      '#442E5A',
+      '#150929',
+      '#03010C',
+      '#427274',
+      '#192B38',
+      '#2E5458',
+      '#4E7367',
+      '#8C0059',
+    ];
+
+    const randomColor = Math.floor(Math.random() * colors.length);
+    return colors[randomColor];
+  }
+
   addName() {
-    if (!this.newName) return;
-    if (this.players.find(p => p === this.newName)) {
-      this.newName = '';
-      return;
+    const isDupePlayer = this.players.some(player => player.name === this.newName);
+    if (!this.newName || isDupePlayer) return;
+
+    // All is well, carry on.
+    const newPlayer = {
+      name: this.newName,
+      color: this.playerColor()
     }
-    this.players.push(this.newName);
+    this.players.push(newPlayer);
     this.newName = '';
     this.savePlayerToLocalStorage(this.players);
   }
 
   removeName(idx: number) {
-    console.log('idx', idx)
+    console.log('1 this players: ', this.players);
     this.players.splice(idx, 1);
+    console.log('2 this players: ', this.players);
     this.savePlayerToLocalStorage(this.players);
     this.nameEl?.nativeElement.focus();
   }
 
   startGame() {
     this.router.navigate(['game']);
+  }
+
+  deleteAllPlayers() {
+    this.players = [];
+    localStorage.removeItem("players");
+  }
+
+  getPlayerColor(i: number) {
+    return this.players[i]['color'];
   }
 
 }
